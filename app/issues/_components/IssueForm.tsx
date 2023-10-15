4fc97a3d371b644cb44e92d0cce20b65c5edc84b
Marios Knl <mariosknl@outlook.com>
2023-10-15 08:06:05 +0300

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -8,7 +8,7 @@ import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
@@ -18,7 +18,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
 	ssr: false,
 });
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
 	const router = useRouter();
@@ -28,7 +28,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 		handleSubmit,
 		formState: { errors },
 	} = useForm<IssueFormData>({
-		resolver: zodResolver(createIssueSchema),
+		resolver: zodResolver(issueSchema),
 	});
 	const [error, setError] = useState("");
 	const [isSubmitting, setIsSubmitting] = useState(false);
@@ -36,7 +36,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 	const onSubmit = handleSubmit(async (data) => {
 		try {
 			setIsSubmitting(true);
-			await axios.post("/api/issues", data);
+			if (issue) await axios.patch("/api/issues/" + issue.id, data);
+			else await axios.post("/api/issues", data);
 			router.push("/issues");
 		} catch (error) {
 			setIsSubmitting(false);
@@ -70,7 +71,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 				/>
 				<ErrorMessage>{errors.description?.message}</ErrorMessage>
 				<Button disabled={isSubmitting}>
-					Submit New Issue {isSubmitting && <Spinner />}
+					{issue ? "Update Issue" : "Submit New Issue"}{" "}
+					{isSubmitting && <Spinner />}
 				</Button>
 			</form>
 		</div>
