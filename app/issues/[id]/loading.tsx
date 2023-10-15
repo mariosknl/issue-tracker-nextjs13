faac97317a8c21d87ad26bd52430c208bfbc970d
Marios Knl <mariosknl@outlook.com>
2023-10-15 07:26:07 +0300

    add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Heading, Flex, Card, Box } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
-	return <div>loading...</div>;
+	return (
+		<Box className="max-w-xl">
+			<Skeleton />
+			<Flex gap="3" my="2">
+				<Skeleton width="5rem" />
+				<Skeleton width="8rem" />
+			</Flex>
+			<Card className="prose" mt="4">
+				<Skeleton count={3} />
+			</Card>
+		</Box>
+	);
 };
 
 export default LoadingIssueDetailPage;
