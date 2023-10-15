594f9b60685c0280c7005a7c11e1fdc6d1d7c1c4
Marios Knl <mariosknl@outlook.com>
2023-10-15 08:55:31 +0300

    Remove duplicate skeletons

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
 	const links = [
 		{ label: "Dashboard", href: "/" },
-		{ label: "Issues", href: "/issues" },
+		{ label: "Issues", href: "/issues/list" },
 	];
 
 	return (
